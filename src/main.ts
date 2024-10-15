cf43faa2ddd6173afbb02a8f2e6c908ba18b1ade
g4mer9 <rileyfinkgamer9@gmail.com>
2024-10-15 16:42:25 -0700

    done w/ step 1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
-const APP_NAME = "changing something so i can publish the fork";
+const APP_NAME = "Drawing Program!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const header = document.createElement("h1");
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.height = 256;
+canvas.width = 256;
+const ctx = canvas.getContext("2d");
+
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+header.innerHTML = APP_NAME;
+app.parentNode?.insertBefore(header, app);
